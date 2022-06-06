8665509457c35a6c7f2c82457c1bf16e88cc381a
Thayaa <61584050+Thayaa@users.noreply.github.com>
2022-06-06 10:34:39 +0700

    Update and rename if.py to main.py

--- if.py
+++
@@ -1,5 +0,0 @@
-# Sumber = https://www.w3schools.com/python/python_conditions.asp
-a = 33
-b = 200
-if b > a:
-  print("b is greater than a")
---
+++ main.py
@@ -0,0 +1,15 @@
+# Sumber = https://www.w3schools.com/python/default.asp
+
+# If
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# Else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")

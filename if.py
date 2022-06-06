60ea66d27fa7caa0c516c790d04173264ce71a85
Thayaa <61584050+Thayaa@users.noreply.github.com>
2022-06-06 09:56:26 +0700

    Create if.py

---
+++ if.py
@@ -0,0 +1,13 @@
+# If statement
+
+orang = 100
+zombie = 100
+
+if orang < zombie:
+    print("Kabur, dunia mau kiamat")
+
+if orang > zombie:
+    print("Mari basmi zombie")
+
+if orang == zombie:
+    print("Mari bersiap berperang")

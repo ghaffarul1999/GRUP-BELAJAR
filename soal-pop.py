58bfd110762f8d37aa8a3b337b45fd11a382eac8
delcode92 <101550592+delcode92@users.noreply.github.com>
2022-06-09 08:06:56 +0700

    Create soal-pop.py

---
+++ soal-pop.py
@@ -0,0 +1,52 @@
+# soal: remove/hapus semua bilangan genap pada list
+
+# input --> [10,5,6,6]
+
+# output --> [5]
+
+# SOLUSI 1: ---------------------------------------------------------------------------------------------------------------------
+
+def removeEvenNum(data): 
+    i=0
+
+    for x in data:
+        if x%2 == 0:
+            
+            data.pop(i)
+            getOddNum( data )
+            
+        else:
+            i+=1
+
+    return data
+
+x = removeEvenNum( [10,5,6,6] )
+
+print(x)
+
+
+# SOLUSI 2: (menggunakan flag True dan False) --------------------------------------------------------------------------------------------
+
+def removeEvenNum(data):
+    i=0
+    stat = False
+
+    for x in data:
+        if x%2 == 0:
+            stat = True
+            break
+            
+        else:
+            i+=1
+
+    if stat == True:
+        data.pop(i)
+        removeEvenNum( data )
+
+    return data
+
+x = removeEvenNum(d)
+
+print( [10,5,6,6] )
+
+

6d2f78a5c5411990b0d8d80a8c1a0331ab3eb47d
MRXAZK <70313408+MRXAZK@users.noreply.github.com>
2022-06-10 21:27:19 +0700

    Delete Test Case 3.py

--- Test Case 3.py
+++
@@ -1,9 +0,0 @@
-
-def sortDictionaryDesc():
-    d = {"ani":80, "budi":70, "ana":55, "susi":75, "rudi":60}
-    return sorted(d.items(),
-        key=lambda x: x[1],
-        reverse=True)
-
-print(sortDictionaryDesc())
-

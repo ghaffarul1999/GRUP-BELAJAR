a67ab7fa3f0d2e255f393c8895a017e07e228fb8
MRXAZK <mrxazk@protonmail.com>
2022-06-13 11:45:00 +0700

    Menambahkan Test Case 4

---
+++ Test Case/TestCase4.py
@@ -0,0 +1,9 @@
+def bonus(salary, bonus):
+    if bonus == True:
+        bonus2 = salary * 10
+        print("bonus sebesar" + bonus2)
+    else:
+        print("Tidak ada bonus")
+
+bonus(150 ,True)
+
